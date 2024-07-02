fe50b6683c62999dce5e8e2bbb765a85530e4caf
BIJAY SHRESTHA <shresthabzay95@gmail.com>
2024-07-02 18:17:10 +0545

    Database Added

--- app/layout.js
+++ app/layout.js
@@ -2,13 +2,18 @@ import "./globals.css";
 import MainHeader from "../components/main-header/main-header";
 
 export const metadata = {
-  title: "NextLevel Food",
+  title: "BIJAY SHRESTHA",
   description: "Delicious meals, shared by a food-loving community.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="../public/images/burger.jpg" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </head>
       <body>
         <MainHeader />
         {children}
